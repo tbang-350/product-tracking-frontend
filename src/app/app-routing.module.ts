116ea8b63746d8c31670e2a2dbf74e35a7021cff
tbang-350 <tbway350@gmail.com>
2022-05-17 09:07:44 +0300

    Add table for viewin data ,not connected to db yet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
+import { ContractorMapComponent } from './contractor-map/contractor-map.component';
+import { ContractorTableComponent } from './contractor-table/contractor-table.component';
 import { ContractorComponent } from './contractor/contractor.component';
+import { EmployeeMapComponent } from './employee-map/employee-map.component';
+import { EmployeeTableComponent } from './employee-table/employee-table.component';
 import { ForbiddenComponent } from './forbidden/forbidden.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
@@ -12,7 +16,11 @@ const routes: Routes = [
   {path: 'admin', component:AdminComponent, canActivate:[AuthGuard], data:{roles:['Admin']}},
   {path: 'contractor', component:ContractorComponent, canActivate:[AuthGuard], data:{roles:['Contractor']}},
   {path: 'login', component:LoginComponent},
-  {path: 'forbidden', component:ForbiddenComponent}
+  {path: 'forbidden', component:ForbiddenComponent},
+  {path: 'view-employee', component:EmployeeTableComponent},
+  {path: 'view-contractor', component:ContractorTableComponent},
+  {path: 'view-employee-map', component:EmployeeMapComponent},
+  {path: 'view-contractor-map', component:ContractorMapComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,21 @@ import { RouterModule } from '@angular/router';
 import { AuthGuard } from './_auth/auth.guard';
 import { AuthInterceptor } from './_auth/auth.interceptor';
 import { UserService } from './_services/user.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EmployeeTableComponent } from './employee-table/employee-table.component';
+import { ContractorTableComponent } from './contractor-table/contractor-table.component';
+import { LayoutModule } from "@angular/cdk/layout";
+import { MatFormFieldModule} from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import {MatInputModule} from '@angular/material/input'
+import { MatTableModule } from '@angular/material/table';
+import { ContractorMapComponent } from './contractor-map/contractor-map.component';
+import { EmployeeMapComponent } from './employee-map/employee-map.component' 
 
 @NgModule({
   declarations: [
@@ -24,7 +39,11 @@ import { UserService } from './_services/user.service';
     LoginComponent,
     HeaderComponent,
     ForbiddenComponent,
-    ContractorComponent
+    ContractorComponent,
+    EmployeeTableComponent,
+    ContractorTableComponent,
+    ContractorMapComponent,
+    EmployeeMapComponent
   ],
   imports: [
     BrowserModule,
@@ -32,7 +51,20 @@ import { UserService } from './_services/user.service';
     FormsModule,
     HttpClientModule,
     ReactiveFormsModule,
-    RouterModule 
+    RouterModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTableModule
+  
   ],
   providers: [
     AuthGuard,
